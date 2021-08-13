4b51c9c1c87a75f2adc47b1ea883cfcfed6fc574
Carlos <cmonge135@gmail.com>
2021-08-12 20:47:04 -0400

    initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Water Ripple Effect 
+
+Github link: https://github.com/sirxemic/jquery.ripples
+
+Bootstrap jQuery link: "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
---
+++ assests/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(){
+
+    "use strict"
+
+    $('.slider').ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.04,
+        interactive: true,
+        // crossOrigin: "",
+    });
+
+});
